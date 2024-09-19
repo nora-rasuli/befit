1f6047dae7e6bcb209fbc01b4cb3d0f9850fcf3d
nora-rasuli <nora.rasuli@gmail.com>
2024-09-18 17:03:26 -0700

    set up mongodb

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors= require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`);
+});
